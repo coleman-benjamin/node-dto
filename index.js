4bdee253c13b02a735a67c56947df49d2e5a2459
Benjamin Coleman <colemanjben@gmail.com>
2018-05-15 13:32:21 -0700

    missed one

--- index.js
+++ index.js
@@ -1,42 +1,32 @@
 const validation = require("./src/validation");
 const mapper = require("./src/map");
 
-const DTO = function() {
+module.exports = {
+    // Labels for types when creating a DTO definition - see /src/type.js for mappings
+    INTEGER : "_INTEGER",
+    FLOAT : "_FLOAT",
+    STRING : "_STRING",
+    BOOLEAN : "_BOOLEAN",
+    OBJECT : "_OBJECT",
+    SERIALIZED_OBJECT : "_SERIALIZED_OBJECT",
 
-	// Labels for types when creating a DTO definition - see /src/type.js for mappings
-	this.INTEGER = "_INTEGER";
-	this.FLOAT = "_FLOAT";
-	this.STRING = "_STRING";
-	this.BOOLEAN = "_BOOLEAN";
-	this.OBJECT = "_OBJECT";
-	this.SERIALIZED_OBJECT = "_SERIALIZED_OBJECT";
-};
+    test : function(input, dtoDefinition, callback) {
+        try {
+            validation.testFieldNames(dtoDefinition, input);
+            validation.testRequired(dtoDefinition, input);
+            validation.testTypes(dtoDefinition, input);
+            callback(null, input);
+        } catch(e) {
+            callback(e);
+        }
+    },
 
-/*
-    Tests input data against the DTO definition, and returns input untouched if all valid.
-*/
-
-DTO.prototype.test = function(input, dtoDefinition, callback) {
-    try {
-        validation.testFieldNames(dtoDefinition, input);
-        validation.testRequired(dtoDefinition, input);
-        validation.testTypes(dtoDefinition, input);
-        callback(null, input);
-    } catch(e) {
-        callback(e);
+    mapTo : function(domainObject, dtoDefinition, map, callback) {
+        try {
+            let mappedDto = mapper.mapTo(domainObject, dtoDefinition, map);
+            callback(null, mappedDto);
+        } catch(e) {
+            callback(e);
+        }
     }
 };
-
-/*
-    Maps
-*/
-DTO.prototype.mapTo = function(domainObject, dtoDefinition, map, callback) {
-    try {
-        let mappedDto = mapper.mapTo(domainObject, dtoDefinition, map);
-        callback(null, mappedDto);
-    } catch(e) {
-        callback(e);
-    }
-};
-
-module.exports = new DTO();
